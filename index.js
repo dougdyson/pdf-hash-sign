8c57b34a582ae362aa9e55f6a5a769035271204d
Unfamous Dyson <dougdyson@gmail.com>
2022-10-23 14:16:56 -0700

    checkpoint commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
