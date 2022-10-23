048de7639e02b51b6370e57e0a31824bff263459
Unfamous Dyson <dougdyson@gmail.com>
2022-10-23 14:49:23 -0700

    oops! should have made new branch!

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const express = require('express');
+const MetaMaskOnboarding = require('@metamask/onboarding');
+const { encrypt, recoverPersonalSignature } = require('@metamask/eth-sig-util');
+
+const { isMetaMaskInstalled } = MetaMaskOnboarding;
 
 const app = express();
 
@@ -8,3 +12,4 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
+
